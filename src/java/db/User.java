4bf38e8819906c20330ac353841729f2b42bd6ff
Samuele <Samuele@Sam-PC>
2012-11-05 19:37:46 +0100

    master merge by samuele 1

---
+++ src/java/db/User.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Samuele
+ */
+public class User implements Serializable{
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    String password, username, role;
+}

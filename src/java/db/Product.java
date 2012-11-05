e4e45d4468d4e7d4e8a50ba7d74975cfe25dd7f9
Samuele <Samuele@Sam-PC>
2012-11-05 19:04:09 +0100

    classi user product order user

--- src/java/db/Product.java
+++ src/java/db/Product.java
@@ -4,6 +4,7 @@
  */
 package db;
 
+import java.io.Serializable;
 import java.util.Date;
 
 
@@ -11,9 +12,81 @@ import java.util.Date;
  *
  * @author Samuele
  */
-public class Product {
+public class Product implements Serializable{
     String name, user, category, um, urlImage;
     int id, quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUm() {
+        return um;
+    }
+
+    public void setUm(String um) {
+        this.um = um;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getCiccio() {
+        return ciccio;
+    }
+
+    public void setCiccio(Date ciccio) {
+        this.ciccio = ciccio;
+    }
     double price;
     Date ciccio;
 }

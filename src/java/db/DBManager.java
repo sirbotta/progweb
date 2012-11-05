85b09b084009cb511e91ac244a90ef5be82d35a7
sirbotta <hipersimon@gmail.com>
2012-11-05 18:43:33 +0100

    prima revision dbmanager

--- src/java/db/DBManager.java
+++ src/java/db/DBManager.java
@@ -4,10 +4,29 @@
  */
 package db;
 
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author simone
  */
-public class DBManager {
+public class DBManager implements Serializable {
+    // transient == non viene serializzato
+
+    private transient Connection con;
+
+    public DBManager(String dburl) throws SQLException {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver", true,
+                    getClass().getClassLoader());
+        } catch (Exception e) {
+            throw new RuntimeException(e.toString(), e);
+        }
+        Connection con = DriverManager.getConnection(dburl);
+        this.con = con;
+    }
     
 }

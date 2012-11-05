55bf9125a56826ee2c62c21f0d3c98292f0d6341
sirbotta <hipersimon@gmail.com>
2012-11-05 20:03:55 +0100

    aggiunto seed per il db e il createDB.sql sotto package db

---
+++ src/java/db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO CATEGORY (name) VALUES('Ortaggi'),('Legumi'),('Frutta'),('Semi');
+
+INSERT INTO USERS (username,password,role) VALUES 
+('ciccio','ciccio','seller'),
+('pino','pino','seller'),
+('mercante','mercante','seller'),
+('elettra','elettra','seller'),
+('canto','canto','buyer'),
+('gino','gino','buyer'),
+('perla','perla','buyer'),
+('scana','scana','buyer'); 

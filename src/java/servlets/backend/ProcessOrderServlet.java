94e3a2b7d8aa2776e1d7bd5b8bbc24b6af5f52a6
sirbotta <hipersimon@gmail.com>
2012-11-30 00:12:48 +0100

    minor fix for release 1.0

--- src/java/servlets/backend/ProcessOrderServlet.java
+++ src/java/servlets/backend/ProcessOrderServlet.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlets.frontend;
+package servlets.backend;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
--- src/java/servlets/frontend/LandingBuyerServlet.java
+++ src/java/servlets/frontend/LandingBuyerServlet.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlets.backend;
+package servlets.frontend;
 
 import db.*;
 import helpers.PageHelper;
